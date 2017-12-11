e0fc1e734aec182679241b3eaa8c64c876e85ab4
Christophe Delaere <christophe.delaere@cern.ch>
2017-12-11 18:00:21 +0100

    make server.py wsgi-ready

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
-from mattermostgithub import app
-try:
+from mattermostgithub import app as application
+
+if __name__ == "__main__":
+  try:
     import config
-except ImportError:
+  except ImportError:
     from mattermostgithub import config
-app.run(
+  app.run(
     host=config.SERVER['address'] or "0.0.0.0",
     port=config.SERVER['port'] or 5000,
     debug=True
-)
+  )
